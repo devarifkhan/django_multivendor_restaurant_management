4d02fd89af9f63ce00f023e2f465131b60528d23
Ariful Islam <arifcse209@gmail.com>
2025-10-26 12:28:50 +0600

    Switch to environment variable-based configuration using python-decouple and add .env.example

--- dishonline_main/settings.py
+++ dishonline_main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h5n50kh^x%wj(a_$gjw9&2-j3_1hz#)h79&jl)vfllvk0l9q(0'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -75,11 +76,11 @@ WSGI_APPLICATION = 'dishonline_main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dishonline_db',
-        'USER': 'dishonline_user',
-        'PASSWORD': 'dishonline_password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT",cast=int),
     }
 }
 
