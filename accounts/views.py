669eedabd0aed6b21218505483895edce9d3e421
Ariful Islam <arifcse209@gmail.com>
2025-10-29 16:09:27 +0600

    Update registerUser view to create user with create_user method and set role to CUSTOMER

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render, redirect
 
 from .forms import UserForm
-
+from .models import User
 
 # Create your views here.
 def registerUser(request):
     if request.method == 'POST':
         forms = UserForm(request.POST)
         if forms.is_valid():
-            user = forms.save(commit=False)
+            # password = forms.cleaned_data['password']
+            # user = forms.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # forms.save()
+            # create the user using create_user method
+            first_name = forms.cleaned_data['first_name']
+            last_name = forms.cleaned_data['last_name']
+            username = forms.cleaned_data['username']
+            email = forms.cleaned_data['email']
+            password = forms.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = User.CUSTOMER
-            forms.save()
+            user.save()
             return redirect('registerUser')
     else:
         forms = UserForm()
