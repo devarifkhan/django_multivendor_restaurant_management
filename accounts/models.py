d7ff2c9eac8391d404a101d8dbd4d97bd44eac85
Ariful Islam <arifcse209@gmail.com>
2025-10-29 12:37:19 +0600

    Implement post_save signal receiver to automatically create or update UserProfile on User save

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 from django.contrib.auth.models import AbstractUser,BaseUserManager
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UserManager(BaseUserManager):
@@ -95,6 +96,13 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.email
 
+@receiver(post_save,sender=User)
 def post_save_create_profile_receiver(sender, instance, created, **kwargs):
-    pass
+    if created:
+        UserProfile.objects.create(user=instance)
+        print("'Profile created'")
+    else:
+        profile = UserProfile.objects.get(user=instance)
+        profile.save()
+        print("'Profile already created'")
 # post_save.connect(post_save_create_profile_receiver, sender=User)
