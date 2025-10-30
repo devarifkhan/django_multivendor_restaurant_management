0f840d01a52128069248c2d7d624ea605676809a
Ariful Islam <arifcse209@gmail.com>
2025-10-30 18:21:00 +0600

    Implement account activation logic and set default from email for outgoing messages

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
@@ -18,6 +19,7 @@ def detectUser(user):
         return redirectUrl
 
 def send_verification_email(request,user):
+    from_email= settings.DEFAULT_FROM_EMAIL
     current_site = get_current_site(request)
     mail_subject = 'Please activate your account'
     message = render_to_string('accounts/emails/account_verification_email.html', {
@@ -27,5 +29,5 @@ def send_verification_email(request,user):
         'token': default_token_generator.make_token(user),
     })
     to_email = user.email
-    mail = EmailMessage(mail_subject, message, to=[to_email])
+    mail = EmailMessage(mail_subject, message,from_email, to=[to_email])
     mail.send()
