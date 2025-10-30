f4111353de7e97993d6da842d71cca5d44b686c4
Ariful Islam <arifcse209@gmail.com>
2025-10-30 10:20:39 +0600

    Add vendor registration view, template, and URL for onboarding vendors

--- accounts/views.py
+++ accounts/views.py
@@ -30,3 +30,30 @@ def registerUser(request):
         'form': forms
     }
     return render(request,'accounts/registerUser.html',context)
+
+def registerVendor(request):
+    if request.method == 'POST':
+        forms = UserForm(request.POST)
+        if forms.is_valid():
+            # password = forms.cleaned_data['password']
+            # user = forms.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.RESTAURANT
+            # forms.save()
+            # create the user using create_user method
+            first_name = forms.cleaned_data['first_name']
+            last_name = forms.cleaned_data['last_name']
+            username = forms.cleaned_data['username']
+            email = forms.cleaned_data['email']
+            password = forms.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user.role = User.RESTAURANT
+            user.save()
+            messages.success(request, 'Your account has been registered successfully.')
+            return redirect('registerVendor')
+    else:
+        forms = UserForm()
+    context = {
+        'form': forms
+    }
+    return render(request, 'accounts/registerVendor.html', context)
