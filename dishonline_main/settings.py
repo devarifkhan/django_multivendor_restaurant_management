ffed1633678a94a7599cd5a307d5d9a0741be9d4
devarifkhan <arifcse209@gmail.com>
2026-02-22 10:01:18 +0600

    Add CSRF_TRUSTED_ORIGINS to settings for enhanced security

--- dishonline_main/settings.py
+++ dishonline_main/settings.py
@@ -30,6 +30,13 @@ DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://dishonline.koyeb.app',
+    'https://*.koyeb.app',
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+]
+
 
 # Application definition
 
